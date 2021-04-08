8026de07fa6b1a1dacd76bc704dfbabb7e20cd6d
Viral <viral.vadgama@live.com>
2021-04-08 21:48:18 +0530

    models created for city country and state

---
+++ api/models/State.js
@@ -0,0 +1,42 @@
+/**
+ * Country.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+ module.exports = {
+
+    attributes: {
+
+        name: {
+            type: 'string',
+            required: true,
+            unique: true,
+            minLength: 3,
+            maxLength: 50
+        },
+
+        country: {
+            required: true,
+            model: 'country'
+        },
+
+        //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+        //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+        //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+        //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+        //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+        //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+        //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+        //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+        //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    },
+
+};
+
